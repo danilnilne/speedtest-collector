ade538e51cde05377bf38565d64fe8a9eb1cde7a
danilnilne <danilnilne@ProgramBase.local>
2024-03-17 16:01:35 +0300

    воскресенье, 17 марта 2024 г. 16:01:35 (MSK)

--- start.py
+++ start.py
@@ -76,7 +76,7 @@ def db_save_result(data, **db_config):
 
         query = ("INSERT INTO speedtest (id, result) "
                  "VALUES (%s, %s)")
-        values = (time.time_ns(), str(data))
+        values = (time.time(), str(data))
 
         print(query, values)
         cursor.execute(query, values)
